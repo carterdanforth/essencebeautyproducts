d62527f3b217fc828e177f9620814f357e6db4b0
colig <colig@192.168.1.4>
2016-04-16 08:06:11 +0800

    added Clicky Tracking Codes and added ticker.php

--- footer.php
+++ footer.php
@@ -16,3 +16,9 @@
         </ul>
     </div>
 </footer>
+<div style="display: none;">
+<a title="Web Statistics" href="http://clicky.com/100943303"><img alt="Web Statistics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100943303); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100943303ns.gif" /></p></noscript>
+</div>
